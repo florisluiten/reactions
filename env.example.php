121d6b34df6f34321b681cb8e392175556244c87
Floris Luiten <floris@florisluiten.nl>
2019-04-29 14:36:00 +0200

    Setup env.example.php file. This can later be used for storing all environment variables and secrets

---
+++ env.example.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Environment variables
+ *
+ * @author  Floris Luiten <floris@florisluiten.nl>
+ * @package Reactions
+ */
+
+declare(strict_types=1);
+
+/**
+ * You can configure your environment here. Note that you should not commit
+ * this file, since it contains your secrets!
+ */
+return array(
+);
