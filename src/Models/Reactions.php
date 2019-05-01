dec875e8257a337ce8fe080292bdd0339fdab95b
Floris Luiten <floris@florisluiten.nl>
2019-05-01 20:56:55 +0200

    Implemented Models/Reactions

---
+++ src/Models/Reactions.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Reactions model
+ *
+ * @author  Floris Luiten <floris@florisluiten.nl>
+ * @package Reactions
+ */
+
+declare(strict_types=1);
+
+namespace Fluiten\Reactions\Models;
+
+use \Fluiten\Reactions as App;
+
+class Reactions
+{
+    /**
+     * @param string The reactionID
+     */
+    public $reactionID;
+
+    /**
+     * @param string The parentID
+     */
+    public $parentID;
+
+    /**
+     * @param string The articleID
+     */
+    public $articleID;
+
+    /**
+     * @param string The score
+     */
+    public $score;
+
+    /**
+     * @param string The userID
+     */
+    public $userID;
+
+    /**
+     * @param string The publish date
+     */
+    public $publishDate;
+
+    /**
+     * @param string The content
+     */
+    public $content;
+
+    /**
+     * Return the query for getting the reactions
+     *
+     * @param \PDO   $database The database handle
+     * @param string $ID       The article ID
+     *
+     * @return \PDOStatement
+     */
+    public function queryByArticle(\PDO $database, string $articleID): ? \PDOStatement
+    {
+        $statement = $database->prepare(
+            'SELECT `reactionID`, `parentID`, `articleID`, `score`, `userID`, `publishDate`, `content` '
+            . 'FROM `reactions` '
+            . 'WHERE articleID = :ID'
+        );
+        $statement->bindParam(':ID', $articleID);
+
+        $statement->setFetchMode(\PDO::FETCH_CLASS, __CLASS__);
+
+        return $statement;
+    }
+}
