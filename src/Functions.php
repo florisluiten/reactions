7fa1cbe7034820664246687687e9e1f1c6529700
Floris Luiten <floris@florisluiten.nl>
2019-05-01 20:47:48 +0200

    Implemented arrayToTree

---
+++ src/Functions.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Global functions
+ *
+ * @author  Floris Luiten <floris@florisluiten.nl>
+ * @package Reactions
+ */
+
+declare(strict_types=1);
+
+namespace Fluiten\Reactions;
+
+/**
+ * Transform an array with ID's and parentID's to a tree.
+ *
+ * @param array[] $array The array with ID's and parentID's
+ * @param integer $parentID The parentID for the current level
+ *
+ * @return array[] with "children" key set to the child elements
+ */
+function arrayToTree(array $array, $parentID = null)
+{
+    $tree = array();
+
+    foreach ($array as $child) {
+        if ($child['parentID'] === $parentID) {
+            $child['children'] = arrayToTree($array, $child['ID']);
+            $tree[] = $child;
+        }
+    }
+
+    return $tree;
+}
